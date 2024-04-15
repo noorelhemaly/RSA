c7d09d47e48862d46670bae6ac78afb5ddfbdc38
noorelhemaly <ne2300013@tkh.edu.eg>
2024-04-15 22:23:15 +0200

    check prime

--- RSA.py
+++ RSA.py
@@ -10,3 +10,19 @@ def generate_prime(bits):
         if prime(p):
             return p
         
+#function to check if a number is prime
+def prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
