6bcd65d360081f54f6e4ad6edb8fefd35cccd2b5
noorelhemaly <ne2300013@tkh.edu.eg>
2024-04-15 22:16:00 +0200

    generate large prime numbers

---
+++ RSA.py
@@ -0,0 +1,12 @@
+# Import necessary libraries
+import random  
+import math  
+import time  
+
+#function to generate a prime number of specified bits
+def generate_prime(bits):
+    while True:
+        p = random.randint 
+        if prime(p):
+            return p
+        
